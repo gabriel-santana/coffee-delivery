550697578d56f64d0870b2a4119304a3b1208b01
Gabriel Santana <gabrielsantana77@hotmail.com>
2023-01-10 23:40:45 -0300

    checkout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,4 @@
+import { MapPinLine, CurrencyDollar } from 'phosphor-react'
 import { Input } from './components/Input'
 import {
   AdressContainer,
@@ -12,20 +13,68 @@ export function Checkout() {
         <AdressContainer>
           <h2>Complete seu pedido</h2>
           <div className="contentAdressContainer">
+            <div className="info">
+              <MapPinLine size={22} />
+              <div className="textInfo">
+                <p className="textInfo1">Endereço de Entrega</p>
+                <p className="textInfo2">
+                  Informe o endereço onde deseja receber seu pedido
+                </p>
+              </div>
+            </div>
             <div className="firstRow">
-              <Input type="text" placeholder="CEP" />
+              <Input type="text" className="cep" placeholder="CEP" />
             </div>
             <div className="secondRow">
               <Input type="text" placeholder="Rua" />
             </div>
             <div className="thirdRow">
-              <Input type="text" placeholder="Número" />
+              <Input type="text" className="number" placeholder="Número" />
               <Input type="text" placeholder="Complemento" />
             </div>
             <div className="fourthRow">
-              <Input type="text" placeholder="Bairro" />
+              <Input
+                type="text"
+                className="neighborhood"
+                placeholder="Bairro"
+              />
               <Input type="text" placeholder="Cidade" />
-              <Input type="text" placeholder="UF" />
+              <Input type="text" className="uf" placeholder="UF" />
+            </div>
+          </div>
+
+          <div className="contentPaymentContainer">
+            <div className="info">
+              <CurrencyDollar size={22} />
+              <div className="textInfo">
+                <p className="textInfo1">Pagamento</p>
+                <p className="textInfo2">
+                  O pagamento é feito na entrega. Escolha a forma que deseja
+                  pagar
+                </p>
+              </div>
+            </div>
+            <div className="selectPayment">
+              <div className="selectPaymentMethod">
+                <label>
+                  <input type="radio" name="paymentMethod" value="credit" />
+                  <span>CARTÃO DE CRÉDITO</span>
+                </label>
+              </div>
+
+              <div className="selectPaymentMethod">
+                <label>
+                  <input type="radio" name="paymentMethod" value="debit" />
+                  <span>CARTÃO DE DÉBITO</span>
+                </label>
+              </div>
+
+              <div className="selectPaymentMethod">
+                <label>
+                  <input type="radio" name="paymentMethod" value="money" />
+                  <span>DINHEIRO</span>
+                </label>
+              </div>
             </div>
           </div>
         </AdressContainer>
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -5,12 +5,28 @@ export const CheckoutContainer = styled.div`
   margin: 6rem auto;
 
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(16rem, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
   column-gap: 2rem;
   row-gap: 2.5rem;
+
+  @media (max-width: 1150px) {
+    & {
+      max-width: 80%;
+    }
+  }
+
+  @media (max-width: 720px) {
+    & {
+      max-width: 90%;
+    }
+  }
 `
 export const AdressContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
   margin-top: 3rem;
+
   h2 {
     margin-bottom: 1rem;
   }
@@ -22,8 +38,29 @@ export const AdressContainer = styled.div`
 
     div + div {
       margin: 1rem 0;
+    }
 
-      color: red;
+    .info {
+      display: flex;
+      gap: 0.5rem;
+
+      color: ${(props) => props.theme['yellow-dark']};
+
+      .textInfo {
+        display: flex;
+        flex-direction: column;
+        gap: 0.125rem;
+
+        .textInfo1 {
+          color: ${(props) => props.theme['base-subtitle']};
+          font-size: 1rem;
+        }
+
+        .textInfo2 {
+          color: ${(props) => props.theme['base-text']};
+          font-size: 0.875rem;
+        }
+      }
     }
 
     input {
@@ -40,7 +77,19 @@ export const AdressContainer = styled.div`
     }
 
     .firstRow {
-      width: 30%;
+      width: 100%;
+      display: flex;
+      gap: 0.75rem;
+
+      .cep {
+        width: 30%;
+
+        @media (max-width: 720px) {
+          & {
+            width: 35%;
+          }
+        }
+      }
     }
 
     .secondRow {
@@ -51,12 +100,107 @@ export const AdressContainer = styled.div`
       width: 100%;
       display: flex;
       gap: 0.75rem;
+
+      .number {
+        width: 43%;
+
+        @media (max-width: 720px) {
+          & {
+            width: 40%;
+          }
+        }
+      }
     }
 
     .fourthRow {
       width: 100%;
       display: flex;
       gap: 0.75rem;
+
+      .neighborhood {
+        width: 60%;
+      }
+
+      .uf {
+        width: 30%;
+      }
+    }
+  }
+
+  .contentPaymentContainer {
+    background-color: ${(props) => props.theme['base-card']};
+    padding: 2.5rem;
+    border-radius: 6px;
+
+    .info {
+      display: flex;
+      gap: 0.5rem;
+
+      color: ${(props) => props.theme.purple};
+
+      .textInfo {
+        display: flex;
+        flex-direction: column;
+        gap: 0.125rem;
+
+        .textInfo1 {
+          color: ${(props) => props.theme['base-subtitle']};
+          font-size: 1rem;
+        }
+
+        .textInfo2 {
+          color: ${(props) => props.theme['base-text']};
+          font-size: 0.875rem;
+        }
+      }
+    }
+
+    .selectPayment {
+      display: flex;
+      width: 100%;
+      margin-top: 1rem;
+      gap: 0.75rem;
+      font-size: 0.75rem;
+
+      .selectPaymentMethod {
+        box-sizing: border-box;
+        float: left;
+        height: 3.1875rem;
+        position: relative;
+        width: 100%;
+      }
+
+      .selectPaymentMethod label {
+        background: ${(props) => props.theme['base-button']} no-repeat center;
+        cursor: pointer;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        border-radius: 6px;
+      }
+
+      .selectPaymentMethod label span {
+        z-index: 1;
+      }
+      .selectPaymentMethod label input[type='radio'] {
+        all: unset;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+      }
+      .selectPaymentMethod label input[type='radio']:checked {
+        background-color: ${(props) => props.theme['purple-light']};
+        border: 1px solid ${(props) => props.theme.purple};
+        border-radius: 6px;
+      }
+      .selectPaymentMethod label input[type='radio']:checked + span {
+        color: ${(props) => props.theme['base-text']};
+      }
     }
   }
 `
